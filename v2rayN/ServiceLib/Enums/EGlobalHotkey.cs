2428acf8f1a8d6df70b27ec38fba4ae9dadf8d6a
Juan Vargas <66626747+v4rgas@users.noreply.github.com>
2025-03-06 20:40:36 -0300

    Update EGlobalHotkey.cs

--- v2rayN/ServiceLib/Enums/EGlobalHotkey.cs
+++ v2rayN/ServiceLib/Enums/EGlobalHotkey.cs
@@ -2,10 +2,4 @@
 {
     public enum EGlobalHotkey
     {
-        ShowForm = 0,
-        SystemProxyClear = 1,
-        SystemProxySet = 2,
-        SystemProxyUnchanged = 3,
-        SystemProxyPac = 4,
-    }
 }
